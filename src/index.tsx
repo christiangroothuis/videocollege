9c02716d305ba82a1e48724ed43db953be41ce77
JuanGrooth <christiangroothuis@pm.me>
2022-01-19 17:20:39 +0100

    Few adjustments and cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./pages/Home";
-import reportWebVitals from "./reportWebVitals";
+import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Presentation } from "./pages/Presentation";
 import { SWRConfig } from "swr";
@@ -19,7 +18,7 @@ ReactDOM.render(
 		>
 			<Router>
 				<Routes>
-					<Route path="/" element={<App />} />
+					<Route path="/" element={<Home />} />
 					<Route path="presentation/:id" element={<Presentation />} />
 					{/* <Route path="player/:id" element={<Player />} /> */}
 				</Routes>
@@ -28,8 +27,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
