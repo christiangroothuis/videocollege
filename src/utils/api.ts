affd1d45356c5de2e36b0d2c0788d867f4119dea
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-03-19 01:28:59 +0100

    Redirect to login when session expired

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -11,9 +11,8 @@ const sfapikey = process.env.REACT_APP_SFAPIKEY;
 const fetcher = async (input: RequestInfo, init?: RequestInit) => {
 	const res = await fetch(input, init);
 
-	if ([401, 403].indexOf(res.status) !== -1) {
-		// window.location.href = `https://videocollege.tue.nl/Mediasite/Login/saml?ReturnUrl=${window.location.href}`;
-		alert(`Status ${res.status}!`);
+	if ([401, 403].includes(res.status)) {
+		window.location.href = `https://videocollege.tue.nl/Mediasite/Login/saml?ReturnUrl=${window.location.href}`;
 	}
 
 	if (!res.ok) {
