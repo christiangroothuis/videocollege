c2122598ccb7b54e3f70c85373d3281b34758c74
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-03-21 01:34:50 +0100

    Reduce amount fetched from api

--- src/pages/Course/Course.tsx
+++ src/pages/Course/Course.tsx
@@ -20,7 +20,7 @@ export function Course() {
         )}] AND (Status:Viewable OR Status:Live OR (Status:Record AND IsLiveEnabled:True) OR (Status:OpenForRecord AND IsLiveEnabled:True)) AND IsApproved:True
         AND (ParentFolderId:${folderId})`,
         orderBy: 'RecordDate asc',
-        amountPerPage: 24,
+        amountPerPage: 12,
     });
     const { data: data2, isLoading: isLoadingLastLectures } = usePresentationSearch({
         query: `ParentFolderId:${folderId} Type:Presentation AirDateTimeUtc:[${dateToString(
@@ -29,7 +29,7 @@ export function Course() {
             new Date()
         )}] AND (Status:Viewable OR Status:Live OR (Status:Record AND IsLiveEnabled:True) OR (Status:OpenForRecord AND IsLiveEnabled:True)) AND IsApproved:True
         AND ParentFolderId:${folderId}`,
-        amountPerPage: 24,
+        amountPerPage: 12,
     });
 
     return (
