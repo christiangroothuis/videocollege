8e2b4167c9e4b4211bcd2363d50e77ca085d2287
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-03-21 03:09:36 +0100

    Change scrollToTop to useLayOut

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
 
