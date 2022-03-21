da2bd103504b735d9a7b26ff5fd7df70172b14a4
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-03-21 13:43:48 +0100

    Fix layout height

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import Nav from '../Nav';
 
 export function Layout({ children }: { children?: React.ReactNode }) {
     return (
-        <div className="flex h-screen w-full">
+        <div className="flex min-h-screen w-full">
             <Nav />
             <div className="ml-20 grow bg-bgprimary p-5">{children}</div>
         </div>
