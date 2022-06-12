7a5f18890425c5c803e63fa98ef08d8f844fcc06
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-06-12 17:43:58 +0200

    Use better formatting

--- src/helpers/getLang.ts
+++ src/helpers/getLang.ts
@@ -1,4 +1,7 @@
 export const getLang = (): string => {
-    if (navigator.languages !== undefined) return navigator.languages[0];
+    if (navigator.languages) {
+        return navigator.languages[0];
+    }
+
     return navigator.language;
 };
